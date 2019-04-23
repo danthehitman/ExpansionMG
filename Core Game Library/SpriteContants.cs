e3a946b724fa8da42c4f4e8f07a38c61cae9e0bf
Dan Frank <dan.frank@hpe.com>
2019-04-22 19:28:40 -0600

    Adding terrain sprites.

--- Core Game Library/SpriteContants.cs
+++ Core Game Library/SpriteContants.cs
@@ -9,7 +9,30 @@ namespace HML.Expansion
     public static class SpriteContants
     {
         public const string DEFAULT_ITEM_SPRITE = "defaultItem";
-
         public const string DEFAULT_RESOURCE_SPRITE = "defaultResource";
+
+        public const string TILE_GRASSLAND = "Grassland";
+        public const string TILE_KEY_DESERT = "T_Desert";
+        public const string TILE_MOUNTAIN = "Mountain";
+        public const string TILE_SAVANNA = "Savanna";
+        public const string TILE_ICE = "Ice";
+        public const string TILE_TUNDRA = "Tundra";
+        public const string TILE_WOODLAND = "Woodland";
+        public const string TILE_BOREAL_FOREST = "BorealForest";
+        public const string TILE_SEASONAL_FOREST = "SeasonalForest";
+        public const string TILE_TEMPERATE_RAINFOREST = "TemperateRainforest";
+        public const string TILE_TROPICAL_RAINFOREST = "TropicalRainforest";
+        public const string TILE_DEEP_WATER = "DeepWater";
+        public const string TILE_SHALLOW_WATER = "ShallowWater";
+        public const string TILE_RIVER = "River";
+
+        public const string TILE_RIVER_BEND = "RiverBendSE";
+        public const string TILE_RIVER_CROSS = "RiverCross";
+        public const string TILE_RIVER_HOR = "RiverHor";
+        public const string TILE_RIVER_TERM_HOR = "RiverTermHorE";
+        public const string TILE_RIVER_TERM_VERT = "RiverTermVertN";
+        public const string TILE_RIVER_T = "RiverTN";
+        public const string TILE_RIVER_VERT = "RiverVert";
+        public const string TILE_RIVER_SINGLE = "RiverSingle";
     }
 }
--- Core Game Library/StringContants.cs
+++ Core Game Library/StringContants.cs
@@ -9,5 +9,8 @@ namespace HML.Expansion
     public static class StringContants
     {
         public const string ALL_PROPERTIES_PROPERTY_NAME = "All";
+        
+        public const string CONTENT_IMAGES_FOLDER = "images";
+        public const string CONTENT_TERRAIN_FOLDER = "terrain";
     }
 }
